5ac58e3384fd8b8039132161ed97de6b838b0c67
Tomek <megatrontomaszdudek@gmail.com>
2019-02-15 14:51:17 +0100

    stub of library to run papermill

--- python/libraries/papermill_runner/run.py
+++ python/libraries/papermill_runner/run.py
@@ -3,4 +3,12 @@
 
 class PapermillRunner:
 
-    def __init__(self, input_file):
+    def __init__(self, input_file, output_dir, execution_id, params):
+        self.input_file = input_file
+        self.output_dir = output_dir
+        self.execution_id = execution_id
+        self.params = params
+
+
+    def run(self):
+        pass
