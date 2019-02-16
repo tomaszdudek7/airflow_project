fb6663fcdb67477f7327c3c7be33ce101dbd6bbd
Tomek <megatrontomaszdudek@gmail.com>
2019-02-16 17:30:38 +0100

    properly untar and unpack result from container

--- airflow/dags/launcher/launcher.py
+++ airflow/dags/launcher/launcher.py
@@ -1,9 +1,36 @@
 import logging
 import docker
+import tarfile
+import json
+import os
+import tempfile
 
 from docker import Client
+from docker.errors import NotFound
 
 log = logging.getLogger(__name__)
+
+
+def untar_file_and_get_result_json(client, container):
+    try:
+        tar_data_stream, stat = client.get_archive(container=container, path='/tmp/result.tgz')
+    except NotFound:
+        return dict()
+
+    with tempfile.NamedTemporaryFile() as tmp:
+        for chunk in tar_data_stream.stream():
+            tmp.write(chunk)
+        tmp.seek(0)
+        with tarfile.open(mode='r', fileobj=tmp) as tar:
+            tar.extractall()
+            tar.close()
+
+    with tarfile.open('result.tgz') as tf:
+        for member in tf.getmembers():
+            f = tf.extractfile(member)
+            result = json.loads(f.read())
+            os.remove('result.tgz')
+            return result
 
 
 def launch_docker_container(**context):
@@ -24,7 +51,10 @@ def launch_docker_container(**context):
             l = next(logs)
             log.info(f"Task log: {l}")
     except StopIteration:
-        pass
+        log.info("Docker has finished!")
+
+    result = untar_file_and_get_result_json(client, container)
+    log.info(f"Result was {result}")
 
     log.info(f"Task ends!")
     my_id = context['my_id']
