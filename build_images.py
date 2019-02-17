8f4d8a05283bcddb1289e28376575208cdd25b64
Tomek <megatrontomaszdudek@gmail.com>
2019-02-17 20:18:04 +0100

    prettyfy buildscript

--- build_images.py
+++ build_images.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import docker
 import argparse
 import logging
@@ -9,41 +11,50 @@ import shutil, errno
 
 LIBRARIES_TO_COPY = ['papermill_runner', 'result_saver']
 
+
 def copy_dirs(src, dst):
     try:
         shutil.copytree(src, dst)
-    except OSError as exc: # python >2.5
+    except OSError as exc:  # python >2.5
         if exc.errno == errno.ENOTDIR:
             shutil.copy(src, dst)
-        else: raise
+        else:
+            raise
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setLevel(logging.DEBUG)
-handler.setFormatter(formatter)
-root.addHandler(handler)
 
-log = logging.getLogger("build_images")
+def get_directories_to_browse(args) -> List[str]:
+    taskname = args.taskname
+    if taskname is not None:
+        log.info(f"Taskname specified as {taskname}. Will build only that docker image.")
+        path = f"./docker/{taskname}"
+        if not os.path.isdir(path):
+            raise Exception(f'''Directory /docker/{taskname} does not exists.''')
+        return [path]
+    else:
+        log.info(f"No particular task name specified. Will build every image in /docker/.")
+        return [x for x in os.listdir('./docker') if not x.startswith('.') and os.path.isdir(f'./docker/{x}')]
+
+
+def configure_and_get_logger() -> logging.Logger:
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    return logging.getLogger("build_images")
+
+
+log = configure_and_get_logger()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--task", dest="taskname",
                     help="If you wish to build only specific task, specify its catalog name", required=False)
 
 args = parser.parse_args()
-
-taskname = args.taskname
-directories = []
-if taskname is not None:
-    log.info(f"Taskname specified as {taskname}. Will build only that docker image.")
-    path = f"./docker/{taskname}"
-    if not os.path.isdir(path):
-        raise Exception(f'''Directory /docker/{taskname} does not exists.''' )
-    directories = [path]
-else:
-    log.info(f"No particular task name specified. Will build every image in /docker/.")
-    directories = [x for x in os.listdir('./docker') if not x.startswith('.') and os.path.isdir(f'./docker/{x}')]
+directories = get_directories_to_browse(args)
 
 log.info(f"Browsing {directories}")
 
@@ -62,8 +73,3 @@ for directory in directories:
             dest = f'./docker/{directory}/{library}'
             log.info(f"Removing {dest}")
             shutil.rmtree(dest)
-
-
-
-
-
